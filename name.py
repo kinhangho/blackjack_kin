833b8ffece0b70933f6bc952d0f133d9e16c6a76
kinhangho <ho.kin.hang1124@gmail.com>
2022-04-20 16:28:30 +0800

    [added classes]

--- name.py
+++ name.py
@@ -8,67 +8,71 @@ deck = [2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9
 player = []
 dealer = []
 
-def dealcard(turn):
-    card = random.choice(deck)
-    turn.append(card)
-    deck.remove(card)
+class Cards:
+    def dealcard(turn):
+        card = random.choice(deck)
+        turn.append(card)
+        deck.remove(card)
 
 
-def total(turn):
-    total = 0
-    face = ["K", "Q", "j"]
-    for card in turn:
-        if card in range(1,11):
-            total += card
-        elif card in face:
-            total += 10
-        else:
-            if total >11:
-                total += 1
+    def total(turn):
+        total = 0
+        face = ["K", "Q", "j"]
+        for card in turn:
+            if card in range(1,11):
+                total += card
+            elif card in face:
+                total += 10
             else:
-                total = 11
-    return total
+                if total >11:
+                    total += 1
+                else:
+                    total = 11
+        return total
 
-def revealdealer():
-    if len(dealer) == 2:
-        return dealer[0]
-    elif len(dealer) >2:
-        return dealer[0], dealer[1]
+class Dealing:
+    def revealdealer():
+        if len(dealer) == 2:
+            return dealer[0]
+        elif len(dealer) >2:
+            return dealer[0], dealer[1]
 
-for _ in range(2):
-    dealcard(dealer)
-    dealcard(player)
-
-while dealerIn or playerIn:
-    print(f"Dealer had {revealdealer()}and X")
-    print(f"You have {player} for a total of {total(player)}")
-    if playerIn:
-        stayOrhit = input("1:Stay\n 2:Hit\n")
-    if total(dealer) >16:
-        dealerIn = False
-    else:
+    for _ in range(2):
         dealcard(dealer)
-    if stayOrhit =='1':
-        playerIn = False
-    else:
         dealcard(player)
-    if total(player) >= 21:
-        break
-    if total(dealer) >= 21:
-        break
+        
+        
 
-if total(player) == 21:
-    print(f"\nYou have {player} for a total of {total(player)} and dealer has {total(dealer)}")
-    print("Blackjack! You are the winner")
-elif total(dealer) == 21:
-    print(F"Good bye!!! Dealer has {total(dealer)}, Blackjack")
-elif total(player) > 21:
-    print("You lose, bust")
-elif total(dealer) > 21:
-    print(f"You win, You have {total(player)} and dealer bust")
-elif 21 - total(dealer) < 21 - total(player):
-    print(f"\nYou have {player} for a total of {total(player)} and dealer has {total(dealer)}")
-    print("Dealer wins!!")
-elif 21 - total(dealer) > 21 - total(player):
-    print(f"\nYou have {player} for a total of {total(player)} and dealer has {total(dealer)}")
-    print("You wins!!")
+    while dealerIn or playerIn:
+        print(f"Dealer had {revealdealer()}and X")
+        print(f"You have {player} for a total of {total(player)}")
+        if playerIn:
+            stayOrhit = input("1:Stay\n 2:Hit\n")
+        if total(dealer) >16:
+            dealerIn = False
+        else:
+            dealcard(dealer)
+        if stayOrhit =='1':
+            playerIn = False
+        else:
+            dealcard(player)
+        if total(player) >= 21:
+            break
+        if total(dealer) >= 21:
+            break
+
+    if total(player) == 21:
+        print(f"\nYou have {player} for a total of {total(player)} and dealer has {total(dealer)}")
+        print("Blackjack! You are the winner")
+    elif total(dealer) == 21:
+        print(F"Good bye!!! Dealer has {total(dealer)}, Blackjack")
+    elif total(player) > 21:
+        print(f"You lose, You have {total(player)} bust")
+    elif total(dealer) > 21:
+        print(f"You win, You have {total(player)} and dealer bust")
+    elif 21 - total(dealer) < 21 - total(player):
+        print(f"\nYou have {player} for a total of {total(player)} and dealer has {total(dealer)}")
+        print("Dealer wins!!")
+    elif 21 - total(dealer) > 21 - total(player):
+        print(f"\nYou have {player} for a total of {total(player)} and dealer has {total(dealer)}")
+        print("You wins!!")
